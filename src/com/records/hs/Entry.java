75a03f767cb85d298a4d26ee5c63b26c8201addb
Logan Kulinski <lbkulinski@users.noreply.github.com>
2020-03-15 22:35:03 -0500

    Initial commit

---
+++ src/com/records/hs/Entry.java
@@ -0,0 +1,11 @@
+package com.records.hs;
+
+/**
+ * An entry in the HS Records application.
+ *
+ * @author Logan Kulinski, lbkulinski@icloud.com
+ * @version March 15, 2020
+ */
+public final class Entry {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * A module declaration for the HS Records application.
+ *
+ * @author Logan Kulinski, lbkulinski@icloud.com
+ */
+module com.records.hs {
+    exports com.records.hs;
+}
